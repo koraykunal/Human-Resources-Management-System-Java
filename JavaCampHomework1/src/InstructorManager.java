f0c92d9b12691c8e6660dcd47732eee46ebff62b
Koray Kunal <76408353+koraykunal@users.noreply.github.com>
2021-05-13 21:01:29 +0300

    Delete JavaCampHomework1 directory

--- JavaCampHomework1/src/InstructorManager.java
+++
@@ -1,6 +0,0 @@
-
-public class InstructorManager {
-	public void append(User user) {
-		System.out.println("Lesson has succesfully added.");
-	}
-}
--- JavaCampHomework1/src/StudentManager.java
+++
@@ -1,6 +0,0 @@
-
-public class StudentManager {
-	public void enroll(User user) {
-		System.out.println("Student has succesfully enrolled.");
-	}
-}
